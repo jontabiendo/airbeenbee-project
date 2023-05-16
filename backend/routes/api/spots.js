22c6a6be01641db20844e23108dd7db487c95c51
Jonathan <jontabiendo@gmail.com>
2023-05-16 17:00:00 -0400

    get all spots complete

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -3,25 +3,55 @@ const bcrypt = require('bcryptjs');
 const { check } = require('express-validator');
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { Spot, SpotImage } = require('../../db/models');
+const { Spot, SpotImage, Review } = require('../../db/models');
 
 const router = express.Router();
 
 router.get('/', async (req, res, next) => {
     const response = {}
     
-    const spots = await Spot.findAll()
-
-    spots.forEach(spot => async () =>{
-        spot.previewImage = await SpotImage.findOne({
-            where: {
-                spotId: spot.id
+    const spots = await Spot.findAll({
+        include: [
+            {
+                model: SpotImage
             },
-            attributes: ['url']            
-        })
+            {
+                model: Review
+            }
+        ]
     })
 
-    res.json({"Spots": spots})
+    const spotList = [] 
+    spots.forEach(spot =>{
+        spotList.push(spot.toJSON());
+    });
+
+    spotList.forEach(spot => {
+        spot.SpotImages.forEach(spotImage => {
+            if (spotImage.preview === true) {
+                spot.previewImage = spotImage.url
+            }
+        })
+        if (!spot.previewImage) {
+            spot.previewImage = 'No preview image found'
+        }
+        delete spot.SpotImages
+    });
+
+    spotList.forEach(spot => {
+        if (spot.Reviews.length) {
+            let count = 0;
+            spot.Reviews.forEach(review => {
+                count += review.stars
+            })
+            spot.avgRating = count/spot.Reviews.length
+        }
+        else spot.avgRating = 'No reviews have been submitted yet'
+
+        delete spot.Reviews
+    })
+
+    res.json({"Spots": spotList})
 })
 
 module.exports = router;
