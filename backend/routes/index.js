04122bdd70c4bd6efec2cbc009dc7e75d72df88c
Jonathan <jontabiendo@gmail.com>
2023-06-02 16:08:23 -0400

    csrfFetch started

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -10,6 +10,33 @@ router.get('/api/csrf/restore', (req, res) => {
     });
 });
 
+if (process.env.NODE_ENV === 'production') {
+    const path = require('path');
+
+    router.get('/', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+
+    router.use(express.static([ath.resolve('../frontend/build')]));
+
+    router.get(/^(?!\/?api).*/, (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.sendFile(
+            path.resolve(__dirname, '../../frontend', 'build', 'index.html')
+        );
+    });
+}
+
+if (process.env.NODE_ENV !== 'production') {
+    router.get('/api/csrf/restore', (req, res) => {
+        res.cookie('XSRF-TOKEN', req.csrfToken());
+        return res.json({});
+    });
+}
+
 router.use('/api', apiRouter)
 
 module.exports = router
